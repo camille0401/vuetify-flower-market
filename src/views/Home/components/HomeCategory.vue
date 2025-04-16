74eeae4cd7e89bd2708024d1bc7e848cdb8cf6b1
wangdery <wangdery819@gmail.com>
2025-04-16 18:04:47 +0800

    home-category-update

--- src/views/Home/components/HomeCategory.vue
+++ src/views/Home/components/HomeCategory.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="home-category" v-show="categoryStore.categoryList.length">
-    <v-sheet color="rgba(0, 0, 0, 0.7)">
+  <div v-show="categoryStore.categoryList.length" class="home-category">
+    <v-sheet color="rgba(0, 0, 0, 0.7)" style="height: 100%;">
       <ul class="menu">
+        <!-- 遍历主分类 -->
         <li v-for="category in categoryStore.categoryList" :key="category.id">
-          <RouterLink class="main-menu" :to="`/category/1/${category.id}`">{{ category.cname }}</RouterLink>
-          <div class="sub-menu">
+          <RouterLink class="main-menu" :to="`/category/1/${category.id}`">
+            {{ category.cname }}
+          </RouterLink>
+          <!-- 子分类 -->
+          <div v-if="category.children.length" class="sub-menu">
             <RouterLink v-for="child in category.children" :key="child.id" :to="`/category/2/${child.id}`">
               {{ child.cname }}
             </RouterLink>
@@ -15,13 +19,14 @@
   </div>
 </template>
 
+
 <script setup>
 import { useCategoryStore } from '@/stores/category'
 
 const categoryStore = useCategoryStore()
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .home-category {
   width: 250px;
   height: 500px;
@@ -29,7 +34,7 @@ const categoryStore = useCategoryStore()
   z-index: 99;
 
   .menu {
-    height: 500px;
+    height: 100%;
     overflow-y: auto;
     scrollbar-width: none;
 
@@ -41,38 +46,36 @@ const categoryStore = useCategoryStore()
     li {
       padding: 10px 20px;
       border-bottom: 1px dashed rgba(255, 255, 255, 0.2);
-
-      a {
-        margin-right: 4px;
-        color: #fff;
-      }
+      transition: background 0.3s ease;
 
       &:hover {
         background: rgb(var(--v-theme-primary-darken-1));
-        transition: all .2s linear;
-
-        a {
-          color: #fff;
-        }
       }
 
       .main-menu {
         font-size: 16px;
         line-height: 50px;
+        color: #fff;
       }
 
       .sub-menu {
         display: flex;
         flex-wrap: wrap;
+        gap: 10px;
 
         a {
           font-size: 14px;
-          padding: 0 5px;
-          padding-left: 0;
           line-height: 24px;
+          color: #fff;
         }
-
       }
+    }
+  }
+
+  @media (max-width: 600px) {
+    .menu {
+      width: 100%;
+      /* 在小屏设备上调整菜单宽度 */
     }
   }
 }
